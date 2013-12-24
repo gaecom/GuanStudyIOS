c6f2740ce6a0ef30fd5185f51f83cefca6b9a053
guanchengyun <Document@Mac-mini.local>
2013-12-24 16:10:26 +0800

    Initial Commit

---
+++ GuanStudyIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GuanStudyIOS
+//
+//  Created by macmini on 13-12-24.
+//  Copyright (c) 2013 城云 官. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
